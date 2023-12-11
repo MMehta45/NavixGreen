9dc8e1f09be0c7bea9f36ecc41a2ba615d3e780b
Tarun <tarunkumar.vijay137@gmail.com>
2023-12-11 08:30:21 -0600

    highlight stuff

--- public/js/index-js.js
+++ public/js/index-js.js
@@ -1,5 +1,21 @@
 // Initialize the tooltip plugin
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  
+//$(document).ready(function () {
+//    $('[data-toggle="tooltip"]').tooltip();
+//  });
+
+gsap.set('span', { width: 0 })
+
+gsap.timeline({
+    scrollTrigger:{
+      trigger: 'span',
+      scrub:0.3,
+      start:"top 40%",
+      end:"top 20%",
+      //markers:true
+    }
+ })
+  .to('span', {
+  width:'100%', 
+  duration:1, ease:'none',
+  stagger:1
+})
