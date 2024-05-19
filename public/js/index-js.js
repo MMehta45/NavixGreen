3eeb5fe5cb59c01b196aea62f08cb1775433615f
Monish <122758729+MMehta45@users.noreply.github.com>
2024-05-18 20:13:53 -0500

    new pages + homepage started

--- public/js/index-js.js
+++ public/js/index-js.js
@@ -42,3 +42,19 @@ gsap.from('.slant-row', {
     });
   },
 });
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
+});
