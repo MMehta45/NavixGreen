e5496fe7398d624f72fe7ad3adb67a1c6746f9fa
AndrewVGustafson <f4andrew7@gmail.com>
2024-05-21 09:33:52 -0500

    added grid layout to the added lightbulb api data

--- public/js/finder-js.js
+++ public/js/finder-js.js
@@ -4,6 +4,8 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+product_count = 0
+
 function get_random_product(){
     $.ajax({
         url: "https://data.energystar.gov/resource/ebgj-qsf7.json",
@@ -13,15 +15,23 @@ function get_random_product(){
     }).done(function(data) {
 
     let rand = Math.floor(Math.random() * data.length);
-
     let model = data[rand];
     console.log(model);
 
     let name = model["brand_name"] + " " + model["model_number"];
 
-    const para = document.createElement("p");
-    para.innerHTML = name;
-    document.getElementById("api-container").appendChild(para);
+    const light_info_div = document.createElement("div");
+    light_info_div.className = "light-info"
+    light_info_div.id = "product" + String(product_count)
+    console.log(light_info_div.id)
+    product_count = product_count + 1
+    
+    const light_name = document.createTextNode(name)
+
+    light_info_div.appendChild(light_name)
+
+    // light_info_div.innerHTML = "hi";
+    document.getElementById("api-container").appendChild(light_info_div);
 
     });
 }
@@ -29,23 +39,3 @@ function get_random_product(){
 window.onload = function() {
     get_random_product();
   };
-
-function AddMultiple() {
-    let img = document.getElementById('img');
-    let imageURLS = [
-        ""
-    ];
-    // Fetch random images and append to the DOM
-    for (let i = 0; i < 5; i++) {
-        let randomImage = new Image();
-        randomImage.src = imageURLS[i];
-        randomImage.alt = "Random Image";
-        randomImage.width = 100;
-        randomImage.height = 100;
-        // Add right and left margin to the image
-        randomImage.style.marginRight = "10px";
-        randomImage.style.marginLeft = "10px";
-        img.appendChild(randomImage);
-    }
-}
-
