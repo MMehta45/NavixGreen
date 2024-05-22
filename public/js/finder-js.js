b37020db69556b51034ba4b350258d07009f7a2f
Andrew G <f4andrew7@gmail.com>
2024-05-22 02:12:43 -0500

    finished light api grid, added refresh content button, it just needs  better styling and formatting

--- public/js/finder-js.js
+++ public/js/finder-js.js
@@ -4,7 +4,6 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-product_count = 0
 
 function get_products(amount) {
     $.ajax({
@@ -20,36 +19,63 @@ function get_products(amount) {
         console.log(model);
 
 
-        const light_info_div = document.createElement("div");
-        let current_div_id = "product" + String(product_count)
-        light_info_div.className = "light-info card"
-        light_info_div.id = current_div_id
+        const light_div = document.createElement("div");
+        light_div.className = "light-info-card card";
         
-        const model_name = document.createElement("p")
-        model_name.innerHTML = model["model_name"] + "\n"
+        const brand_name = document.createElement("h2");
+        brand_name.innerHTML = model["brand_name"];
+        brand_name.className = "brand-name card-title";
 
-        const light_name = document.createElement("p")
-        light_name.innerHTML = model["brand_name"] + "\n"
+        const model_name = document.createElement("h4");
+        model_name.innerHTML = model["model_name"];
+        model_name.className = "model-name card-title";
 
-        const model_number = document.createElement("p")
-        model_number.innerHTML = model["model_number"] + "\n"
-        
-        const brightness = document.createElement("p")
-        brightness.innerHTML = model["brightness_lumens"]
 
-        light_info_div.appendChild(light_name)
-        light_info_div.appendChild(model_name)
-        light_info_div.appendChild(model_number)
-        light_info_div.appendChild(brightness)
-        product_count ++
+        const light_data_div = document.createElement("div");
+        light_data_div.className = "light-data-grid card-body";
 
-        document.getElementById("api-container").appendChild(light_info_div);
+        let light_data = [
+            ["Model #", model["model_number"]],
+            ["Brightness", model["brightness_lumens"] + " lumens"],
+            ["Efficacy", model["efficacy_lumens_watt"] + " lumens / watt"],
+            ["Wattage", model["wattage_equivalency_watts"] + " watts"]
+        ];
+
+        light_data.forEach(element => {
+            let header = element[0];
+            let data = element[1];
+
+            const current_header = document.createElement("h3");
+            current_header.innerHTML = header;
+            current_header.className = "light-header";
+
+            const current_data = document.createElement("p");
+            current_data.innerHTML = data;
+            current_data.className = "light-data";
+
+            light_data_div.appendChild(current_header);
+            light_data_div.appendChild(current_data);
+        });
+
+        light_div.appendChild(brand_name);
+        light_div.appendChild(model_name);
+        light_div.appendChild(light_data_div);
+
+        document.getElementById("light-card-grid").appendChild(light_div);
     }
     });
 }
 
 window.onload = function() {
     // for (let i = 0; i < 8; i++) {
-    get_products(15);
+    get_products(9);
     // }
   };
+
+function refresh_data() {
+    let grid_div = document.getElementById("light-card-grid");
+    while(grid_div.firstChild){
+        grid_div.removeChild(grid_div.firstChild);
+    }
+    get_products(9)
+};
